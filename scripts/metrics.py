df2c4032ec76e3986e69b0a21eb38c481c183532
Ceder Dens <cederdens@gmail.com>
2025-08-19 09:15:27 +0200

    Plotting the train_subsets grid

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -1,3 +1,6 @@
+import os
+
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -61,7 +64,51 @@ def plot_training_metrics(csv_path):
     plt.show()
 
 
-if __name__ == "__main__":
-    plot_training_metrics(
-        "logs/casanovo_train_subsets/1s_100000p/csv_logs/metrics.csv"
+def plot_train_subsets_grid(root_dir="logs/casanovo_train_subsets"):
+    heatmap = np.full((5, 5), np.nan)
+
+    spectra = []
+    peptides = []
+    vals = {}
+    for d in os.listdir(root_dir):
+        spec, pep = d.split("_")
+        spec = int(spec[:-1])
+        pep = int(pep[:-1])
+        spectra.append(spec)
+        peptides.append(pep)
+        metrics_df = pd.read_csv(
+            os.path.join(root_dir, d, "csv_logs", "metrics.csv")
+        )
+        val = metrics_df["valid_CELoss"].min()
+        if val > 0.6:
+            val = np.nan
+        vals[pep, spec] = val
+
+    spectra = sorted(set(spectra))
+    peptides = sorted(set(peptides))
+    for s in spectra:
+        for p in peptides:
+            val = vals[p, s]
+            heatmap[peptides.index(p), spectra.index(s)] = val
+    plt.figure(figsize=(6, 5))
+    im = plt.imshow(
+        heatmap,
+        origin="lower",
+        aspect="auto",
+        cmap="viridis_r",
     )
+
+    plt.colorbar(im, label="Min validation loss")
+    plt.xticks(ticks=range(len(spectra)), labels=spectra)
+    plt.yticks(ticks=range(len(peptides)), labels=peptides)
+    plt.xlabel("# Spectra")
+    plt.ylabel("# Unique peptides")
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    # plot_training_metrics(
+    #     "logs/casanovo_train_subsets/1s_100000p/csv_logs/metrics.csv"
+    # )
+    plot_train_subsets_grid()
